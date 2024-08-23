f3b071e40cd59794447f7a1f65254806f328ff63
jdelano <jdelano@cedarville.edu>
2024-08-23 11:35:24 -0400

    Initial Commit

---
+++ Restaurant Reviews/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  Restaurant Reviews
+//
+//  Created by John Delano on 8/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "bird.fill")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+                .font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
+            Text("He gives strength to the weary and increases the power of the weak. Even youths grow tired and weary, and young men stumble and fall; but those who hope in the Lord will renew their strength. They will soar on wings like eagles; they will run and not grow weary, they will walk and not be faint.")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(Color.blue)
+                .multilineTextAlignment(/*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                .padding()
+                .lineSpacing(/*@START_MENU_TOKEN@*/5.0/*@END_MENU_TOKEN@*/)
+                .rotation3DEffect(Angle(degrees: 30), axis: (x: 1.0, y: 0.0, z: 0.0))
+                .shadow(color: .gray, radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/, x: 0, y: 5)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
